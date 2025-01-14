5788b53ed1cf2a3cd7051471461c5e612067b2f3
Evgeny Boger <boger@wirenboard.com>
2025-01-14 19:40:41 +0300

    add support for wildcard delegate patterns
    It's useful to delegate patterns like _acme-challenge.*.xip.example.com

--- xip/xip.go
+++ xip/xip.go
@@ -18,6 +18,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/gobwas/glob"
 	"golang.org/x/net/dns/dnsmessage"
 )
 
@@ -822,14 +823,26 @@ func IsAcmeChallenge(fqdnString string) bool {
 	return false
 }
 
+func IsDelegateDomainMatched(fqdnStringLowerCased string, domain string) bool {
+	// the "." prevents "where.com" from being mistakenly recognized as a subdomain of "here.com"
+	if strings.HasSuffix(fqdnStringLowerCased, "."+domain) || fqdnStringLowerCased == domain {
+		return true
+	} else {
+		g, err := glob.Compile(domain, '.')
+		if err == nil && g.Match(fqdnStringLowerCased) {
+			return true
+		}
+	}
+
+	return false;
+}
 func IsDelegated(fqdnString string) bool {
 	fqdnStringLowerCased := strings.ToLower(fqdnString)
 	for domain := range Customizations {
 		if Customizations[domain].NS == nil { // no nameserver? then it can't be delegated
 			continue
 		}
-		// the "." prevents "where.com" from being mistakenly recognized as a subdomain of "here.com"
-		if strings.HasSuffix(fqdnStringLowerCased, "."+domain) || fqdnStringLowerCased == domain {
+		if IsDelegateDomainMatched(fqdnStringLowerCased, domain) {
 			return true
 		}
 	}
@@ -848,8 +861,7 @@ func (x *Xip) NSResources(fqdnString string) []dnsmessage.NSResource {
 		if Customizations[domain].NS == nil { // no nameserver? then it can't be delegated
 			continue
 		}
-		// the "." prevents "where.com" from being mistakenly recognized as a subdomain of "here.com"
-		if strings.HasSuffix(fqdnStringLowerCased, "."+domain) || fqdnStringLowerCased == domain {
+		if IsDelegateDomainMatched(fqdnStringLowerCased, domain) {
 			return Customizations[domain].NS
 		}
 	}
